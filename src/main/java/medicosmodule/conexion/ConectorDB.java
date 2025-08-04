d458c7b29466a7cf29db53280adaa866465653ee
David Pilataxi <david.pilataxi@epn.edu.ec>
2025-08-04 06:52:59 -0500

    nada

--- src/main/java/medicosmodule/conexion/ConectorDB.java
+++ src/main/java/medicosmodule/conexion/ConectorDB.java
@@ -10,7 +10,7 @@ import java.sql.SQLException;
 
 public class ConectorDB {
 
-   private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=HospitalDB;encrypt=true;trustServerCertificate=true;";
+   private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=polisalud;encrypt=true;trustServerCertificate=true;";
     private static final String USER = "sa";
     private static final String PASSWORD = "1234";
 
